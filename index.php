5afb0ef599b0a1d0fec7c9231ca65cb30597aa66
calascionec <chris.calascione@gmail.com>
2017-09-04 16:05:53 -0400

    add initial files, begin work on mobile hero and product add

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php include('templates/head.php'); ?>
+    </head>
+    <body>
+        <?php include('templates/hero.php'); ?>
+    </body>
+
+</html>
---
+++ templates/hero.php
@@ -0,0 +1,34 @@
+<div class="grid grid--container grid--xs-no-gutters grid--sm-add-gutters">
+    <article class="hero" style="background-image: url('/assets/images/hero.jpg')">
+        <div class="hero__text-container">
+            <h1 class="hero__title-container--two-lines">
+                <span class="hero__title hero__header">HIGH EFFICIENCY</span>
+                <br />
+                <span class="hero__title hero__header">HIGH BAY LIGHTING.</span>
+            </h1>
+            <div class="text--center">
+                <h2>
+                    <span class="hero__sub-title hero__header">NOT SO HIGH PRICES.</span>
+                </h2>
+            </div>
+        </div>
+    </article>
+</div>
+
+
+<div class="grid grid--container">
+    <div class="product-add">
+        <h2 class="product-add__title">
+            <span>SUMMER</span><span>SALE</span>
+        </h2>
+        <div class="product-add__container">
+            <div class="product-add__image-container">
+                <img class="img-responsive" src="/assets/images/summersale.png" alt="3 lights">
+            </div>
+            <div class="product-add__text-container">
+                <p class="product-add__text">All Hight Bay lighting</p>
+                <p class="product-add__link-text">UP TO 30% OFF ></p>
+            </div>
+        </div>
+    </div>
+</div>

c6119831768b4dc6899780ff82efcfa8a2afb01e
Chris Calascione <ccalascione@riversagency.com>
2017-09-06 13:53:53 -0400

    desktop hovers for deals

--- templates/deals.php
+++ templates/deals.php
@@ -17,25 +17,8 @@
                                 <div class="product-item__price">$139.99</div>
                                 <div class="product-item__was">WAS $179.99</div>
                             </div>
-                        </div>
-                        <div class="product-item__savings">
-                            <div class="product-item__savings-amount">SAVE $40</div>
-                        </div>
-                    </a>
-                </article>
-            </div>
-
-            <div class="col col--xs-12 col--xs-flex col--sm-6 col--md-3">
-                <article class="product-item product-deal">
-                    <a class="product-item__link" href="#">
-                        <div class="product-deal__content-container">
-                            <div class="product-item__image-container">
-                                <img class="img-responsive" src="/assets/images/product.png" alt="">
-                            </div>
-                            <h3 class="product-item__title">LED Dusk to Dawn E-DD1L Series 500K</h3>
-                            <div class="product-item__price-wrapper">
-                                <div class="product-item__price">$139.99</div>
-                                <div class="product-item__was">WAS $179.99</div>
+                            <div class="product-item__add-to-cart-container">
+                                <button class="button button--full-width" href="#">ADD TO CART</button>
                             </div>
                         </div>
                         <div class="product-item__savings">
@@ -57,6 +40,9 @@
                                 <div class="product-item__price">$139.99</div>
                                 <div class="product-item__was">WAS $179.99</div>
                             </div>
+                            <div class="product-item__add-to-cart-container">
+                                <button class="button button--full-width" href="#">ADD TO CART</button>
+                            </div>
                         </div>
                         <div class="product-item__savings">
                             <div class="product-item__savings-amount">SAVE $40</div>
@@ -76,6 +62,32 @@
                             <div class="product-item__price-wrapper">
                                 <div class="product-item__price">$139.99</div>
                                 <div class="product-item__was">WAS $179.99</div>
+                            </div>
+                            <div class="product-item__add-to-cart-container">
+                                <button class="button button--full-width" href="#">ADD TO CART</button>
+                            </div>
+                        </div>
+                        <div class="product-item__savings">
+                            <div class="product-item__savings-amount">SAVE $40</div>
+                        </div>
+                    </a>
+                </article>
+            </div>
+
+            <div class="col col--xs-12 col--xs-flex col--sm-6 col--md-3">
+                <article class="product-item product-deal">
+                    <a class="product-item__link" href="#">
+                        <div class="product-deal__content-container">
+                            <div class="product-item__image-container">
+                                <img class="img-responsive" src="/assets/images/product.png" alt="">
+                            </div>
+                            <h3 class="product-item__title">LED Dusk to Dawn E-DD1L Series 500K</h3>
+                            <div class="product-item__price-wrapper">
+                                <div class="product-item__price">$139.99</div>
+                                <div class="product-item__was">WAS $179.99</div>
+                            </div>
+                            <div class="product-item__add-to-cart-container">
+                                <button class="button button--full-width" href="#">ADD TO CART</button>
                             </div>
                         </div>
                         <div class="product-item__savings">

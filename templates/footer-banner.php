0c51c58d3d5470d73b4628f78fd7cfe08f5b63ad
Chris Calascione <ccalascione@riversagency.com>
2017-09-06 14:51:52 -0400

    updates for desktop

--- templates/footer-banner.php
+++ templates/footer-banner.php
@@ -1,39 +1,41 @@
-<article class="footer-banner" style="background-image: url('/assets/images/blue-footer-banner.png')">
-    <div class="grid grid--container">
-        <div class="row">
-            <div class="col col--xs-12 text--center">
-                <h2 class="section__title">At e-conolgiht, we see LED lighting differently.</h2>
-                <div class="footer-banner__copy-container">
-                    <p>
-                        LED has revolutionized commercial and residential lighting with its energy efficiency, ease of maintenance and long, reliable product life. At e-conolight, we're out to revolutionize how contractors, builders and homeowners buy LED lighting and accessorires by making it easy to get the exact product they need, fast.
-                    </p>
-                    <p>
-                        Our goal is to offer the very best selection of LED lighting at the very best prices with world-class customer service, fast shipping, easy returns and a can-do attitude.
-                    </p>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col col--xs-12 col--sm-6 col--xs-flex">
-                <div class="callout" style="background-image: url('/assets/images/truck.png')">
-                    <div class="callout__header">
-                        MORE WAYS TO SAVE
-                    </div>
-                    <div class="callout__text">
-                        FREE Shipping Over $500
+<section class="section">
+    <div class="footer-banner" style="background-image: url('/assets/images/blue-footer-banner.png')">
+        <div class="grid grid--container">
+            <div class="row">
+                <div class="col col--xs-12 text--center">
+                    <h2 class="section__title">At e-conolgiht, we see LED lighting differently.</h2>
+                    <div class="footer-banner__copy-container">
+                        <p>
+                            LED has revolutionized commercial and residential lighting with its energy efficiency, ease of maintenance and long, reliable product life. At e-conolight, we're out to revolutionize how contractors, builders and homeowners buy LED lighting and accessorires by making it easy to get the exact product they need, fast.
+                        </p>
+                        <p>
+                            Our goal is to offer the very best selection of LED lighting at the very best prices with world-class customer service, fast shipping, easy returns and a can-do attitude.
+                        </p>
                     </div>
                 </div>
             </div>
-            <div class="col col--xs-12 col--sm-6 col--xs-flex">
-                <div class="callout" style="background-image: url('/assets/images/box.png')">
-                    <div class="callout__header">
-                        90-DAYS RETURNS
+            <div class="row">
+                <div class="col col--xs-12 col--sm-6 col--xs-flex">
+                    <div class="callout" style="background-image: url('/assets/images/truck.png')">
+                        <div class="callout__header">
+                            MORE WAYS TO SAVE
+                        </div>
+                        <div class="callout__text">
+                            FREE Shipping Over $500
+                        </div>
                     </div>
-                    <div class="callout__text">
-                        No Restocking Fees
+                </div>
+                <div class="col col--xs-12 col--sm-6 col--xs-flex">
+                    <div class="callout" style="background-image: url('/assets/images/box.png')">
+                        <div class="callout__header">
+                            90-DAYS RETURNS
+                        </div>
+                        <div class="callout__text">
+                            No Restocking Fees
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</article>
+</section>
--- templates/homepage-grid-ctas.php
+++ templates/homepage-grid-ctas.php
@@ -1,101 +1,95 @@
-<!-- <div class="grid grid--container">
-    <h1>Heading H1</h1>
-    <h2>Heading H2</h2>
-    <h3>Heading H3</h3>
-    <h4>Heading H4</h4>
-    <h5>Heading H5</h5>
-</div> -->
-
-<div class="grid grid--container">
-    <div class="row">
-        <div class="col col--xs-12">
-            <div class="row">
-                <div class="col col--xs-12">
-                    <div class="cta-ad cta-ad--product">
-                        <a class="cta-ad__link-product cta-ad__link" href="#">
-                            <div class="cta-ad__copy-container">
-                                <div class="cta-ad__title-large">
-                                    Landscpe lighting with a twist
+<section class="section">
+    <div class="grid grid--container">
+        <div class="row">
+            <div class="col col--xs-12 col--lg-9">
+                <div class="row">
+                    <div class="col col--xs-12">
+                        <div class="cta-ad cta-product">
+                            <a class="cta-product__link cta-ad__link" href="#">
+                                <div class="cta-product__image-container">
+                                    <img src="/assets/images/products.png" alt="Product Image">
                                 </div>
-                                <p class="cta-ad__copy">Elements TM LED directional floodlights lead the way with 110 delivered lumens and a 10-year warranty.</p>
-                                <span class="cta-ad__link-text">LEARN MORE ></span>
-                            </div>
-                            <div class="cta-ad__product-container">
-                                <img src="/assets/images/products.png" alt="Product Image">
-                            </div>
-                        </a>
+                                <div class="cta-product__copy-container">
+                                    <div class="cta-product__title">
+                                        Landscpe lighting with a twist
+                                    </div>
+                                    <p class="cta-ad__copy--white cta-ad__copy--xtra-bottom-margin cta-ad__copy">Elements TM LED directional floodlights lead the way with 110 delivered lumens and a 10-year warranty.</p>
+                                    <span class="cta-ad__link-text">LEARN MORE ></span>
+                                </div>
+                            </a>
+                        </div>
                     </div>
-                </div>
-                <div class="col col--xs-6">
-                    <div class="cta-ad" style="background-image: url('/assets/images/recessed-lighting.jpg')">
-                        <a class="cta-ad__link-small cta-ad__link" href="#">
-                            <div class="centering-div">
-                                <span class="cta-ad__title-small">Recessed</span>
-                            </div>
-                        </a>
+                    <div class="col col--xs-6 col--lg-4">
+                        <div class="cta-ad" style="background-image: url('/assets/images/recessed-lighting.jpg')">
+                            <a class="cta-ad__link-small cta-ad__link" href="#">
+                                <div class="centering-div">
+                                    <span class="cta-ad__title-small">Recessed</span>
+                                </div>
+                            </a>
+                        </div>
                     </div>
-                </div>
-                <div class="col col--xs-6">
-                    <div class="cta-ad" style="background-image: url('/assets/images/recessed-lighting.jpg')">
-                        <a class="cta-ad__link-small cta-ad__link" href="#">
-                            <div class="centering-div">
-                                <span class="cta-ad__title-small">Parking Garage</span>
-                            </div>
-                        </a>
+                    <div class="col col--xs-6 col--lg-4">
+                        <div class="cta-ad" style="background-image: url('/assets/images/recessed-lighting.jpg')">
+                            <a class="cta-ad__link-small cta-ad__link" href="#">
+                                <div class="centering-div">
+                                    <span class="cta-ad__title-small">Parking Garage</span>
+                                </div>
+                            </a>
+                        </div>
                     </div>
-                </div>
-                <div class="col col--xs-6">
-                    <div class="cta-ad" style="background-image: url('/assets/images/recessed-lighting.jpg')">
-                        <a class="cta-ad__link-small cta-ad__link" href="#">
-                            <div class="centering-div">
-                                <span class="cta-ad__title-small">Dusk to Dawn</span>
-                            </div>
-                        </a>
+                    <div class="col col--xs-6 col--lg-4">
+                        <div class="cta-ad" style="background-image: url('/assets/images/recessed-lighting.jpg')">
+                            <a class="cta-ad__link-small cta-ad__link" href="#">
+                                <div class="centering-div">
+                                    <span class="cta-ad__title-small">Dusk to Dawn</span>
+                                </div>
+                            </a>
+                        </div>
                     </div>
-                </div>
-                <div class="col col--xs-6">
-                    <div class="cta-ad" style="background-image: url('/assets/images/recessed-lighting.jpg')">
-                        <a class="cta-ad__link-small cta-ad__link" href="#">
-                            <div class="centering-div">
-                                <span class="cta-ad__title-small">Track</span>
-                            </div>
-                        </a>
+                    <div class="col col--xs-6 col--lg-4">
+                        <div class="cta-ad" style="background-image: url('/assets/images/recessed-lighting.jpg')">
+                            <a class="cta-ad__link-small cta-ad__link" href="#">
+                                <div class="centering-div">
+                                    <span class="cta-ad__title-small">Track</span>
+                                </div>
+                            </a>
+                        </div>
                     </div>
-                </div>
-                <div class="col col--xs-6">
-                    <div class="cta-ad" style="background-image: url('assets/images/recessed-lighting.jpg')">
-                        <a class="cta-ad__link-small cta-ad__link" href="#">
-                            <div class="centering-div">
-                                <span class="cta-ad__title-small">Wall Pack</span>
-                            </div>
-                        </a>
+                    <div class="col col--xs-6 col--lg-4">
+                        <div class="cta-ad" style="background-image: url('assets/images/recessed-lighting.jpg')">
+                            <a class="cta-ad__link-small cta-ad__link" href="#">
+                                <div class="centering-div">
+                                    <span class="cta-ad__title-small">Wall Pack</span>
+                                </div>
+                            </a>
+                        </div>
                     </div>
-                </div>
-                <div class="col col--xs-6">
-                    <div class="cta-ad" style="background-image: url('/assets/images/recessed-lighting.jpg')">
-                        <a class="cta-ad__link-small cta-ad__link" href="#">
-                            <div class="centering-div">
-                                <span class="cta-ad__title-small">Fluorescent</span>
-                            </div>
-                        </a>
+                    <div class="col col--xs-6 col--lg-4">
+                        <div class="cta-ad" style="background-image: url('/assets/images/recessed-lighting.jpg')">
+                            <a class="cta-ad__link-small cta-ad__link" href="#">
+                                <div class="centering-div">
+                                    <span class="cta-ad__title-small">Fluorescent</span>
+                                </div>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="col col--xs-12">
-            <div class="cta-ad" style="background-image: url('/assets/images/hallway.jpg')">
-                <div class="max-width-60">
-                    <h3 class="highlighted-text__title-container--two-lines">
-                        <span class="highlighted-text__title highlighted-text">ACCENT ON</span>
-                        <br />
-                        <span class="highlighted-text__title highlighted-text highlighted-text--align-left">EFFICIENCY</span>
-                    </h3>
+            <div class="col col--xs-12 col--lg-3 col--lg-flex">
+                <div class="cta-ad" style="background-image: url('/assets/images/hallway.jpg')">
                     <div class="cta-ad__content-container">
-                        <p class="cta-ad__copy">Crisp white LED makes every kitchen more appetizing</p>
-                        <a class="button button--white" href="#">SHOP UNDER CABINET</a>
+                        <h3 class="highlighted-text__title-container--two-lines">
+                            <span class="highlighted-text__title highlighted-text highlighted-text--orange">ACCENT ON</span>
+                            <br />
+                            <span class="highlighted-text__title highlighted-text highlighted-text--orange">EFFICIENCY</span>
+                        </h3>
+                        <div class="cta-ad__copy-container">
+                            <p class="cta-ad__copy">Crisp white LED makes every kitchen more appetizing</p>
+                            <a class="button button--white" href="#">SHOP UNDER CABINET</a>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
+</section>

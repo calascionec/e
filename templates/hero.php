821b9522672d764a7df215cccdc3c6bf87498c19
Chris Calascione <ccalascione@riversagency.com>
2017-09-06 10:03:11 -0400

    responsive styles for hero

--- templates/hero.php
+++ templates/hero.php
@@ -7,9 +7,25 @@
                 <span class="highlighted-text__title highlighted-text highlighted-text--align-right">HIGH BAY LIGHTING.</span>
             </h1>
             <div class="text--center">
-                <h2>
+                <h2 class="hero__subtitle">
                     <span class="highlighted-text__sub-title highlighted-text">NOT SO HIGH PRICES.</span>
                 </h2>
+            </div>
+        </div>
+        <div class="product-ad__outer-container">
+            <div class="product-ad product-ad__desktop">
+                <h2 class="product-ad__title">
+                    <span>SUMMER</span><span>SALE</span>
+                </h2>
+                <div class="product-ad__inner-container">
+                    <div class="product-ad__image-container">
+                        <img class="img-responsive" src="/assets/images/summersale.png" alt="3 lights">
+                    </div>
+                    <div class="product-ad__text-container">
+                        <p class="product-ad__text">All Hight Bay lighting</p>
+                        <p class="product-ad__link-text">UP TO 30% OFF ></p>
+                    </div>
+                </div>
             </div>
         </div>
     </article>
@@ -17,11 +33,11 @@
 
 
 <div class="grid grid--container">
-    <div class="product-ad">
+    <div class="product-ad product-ad__mobile">
         <h2 class="product-ad__title">
             <span>SUMMER</span><span>SALE</span>
         </h2>
-        <div class="product-ad__container">
+        <div class="product-ad__inner-container">
             <div class="product-ad__image-container">
                 <img class="img-responsive" src="/assets/images/summersale.png" alt="3 lights">
             </div>

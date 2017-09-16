cc3d51d50e230423d3b5173ba945aa0b3b9571c8
calascionec <chris.calascione@gmail.com>
2017-09-16 13:44:03 -0400

    make page title template

--- index.php
+++ index.php
@@ -5,14 +5,15 @@
     </head>
     <body>
         <?php include('templates/header.php'); ?>
-        <?php include('templates/banner.php'); ?>
-        <?php include('templates/hero.php'); ?>
-        <?php include('templates/deals.php'); ?>
-        <?php include('templates/lighting-layout.php'); ?>
-        <?php include('templates/homepage-grid-ctas.php'); ?>
-        <?php include('templates/recently-viewed.php'); ?>
-        <?php include('templates/footer-banner.php'); ?>
-        <?php include('templates/alternate-cta.php'); ?>
+        <?php include('templates/page-title.php') ?>
+        <?php //include('templates/banner.php'); ?>
+        <?php //include('templates/hero.php'); ?>
+        <?php //include('templates/deals.php'); ?>
+        <?php //include('templates/lighting-layout.php'); ?>
+        <?php //include('templates/homepage-grid-ctas.php'); ?>
+        <?php //include('templates/recently-viewed.php'); ?>
+        <?php //include('templates/footer-banner.php'); ?>
+        <?php //include('templates/alternate-cta.php'); ?>
     </body>
 
 </html>
